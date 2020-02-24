c0029f65de6b46332245a4ad9a92014f385fda8a
Alfonz <m.alfonz@gmail.com>
2020-02-24 23:40:40 +0800

    Initial commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-teal-400">hello</h1>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
